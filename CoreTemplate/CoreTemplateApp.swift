d88c40667ed4e7e723f0ef1e16e65ad066a595dc
Kranti <darkphoton20@gmail.com>
2024-04-29 18:29:53 +0800

    Initial Commit

---
+++ CoreTemplate/CoreTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreTemplateApp.swift
+//  CoreTemplate
+//
+//  Created by Kranti on 2024/4/29.
+//
+
+import SwiftUI
+
+@main
+struct CoreTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
